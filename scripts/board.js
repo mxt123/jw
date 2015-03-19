06039d2486a5865107db39e10b46f40b4fe7d91d
mxt123 <mxtose@gmail.com>
2015-03-19 09:42:14 +0000

    4-14

--- scripts/board.js
+++ scripts/board.js
@@ -119,6 +119,51 @@ jewel.board = (function(){
 		return (dx + dy === 1);
 	}
 
+	function getChains() {
+		var x, y, chains = [];
+
+		for (x = 0l x < cols; x++) {
+			chains[x] = [];
+			for (y = 0; y < rows; y++) {
+				chains[x][y] = checkChain(x,y);
+			}
+		}
+		return chains;
+	}
+
+	function check() {
+		var chains = getChains(),
+			hadChains = false,
+			score = 0,
+			removed = [],
+			moved = [],
+			gaps = [];
+
+		for (var x = 0; x < cols; x++) {
+			gaps[x] = 0;
+			for (var y = rows-1; y >=0; y ++) {
+				if (chains[x][y] > 2) {
+					hadChains = true;
+					gaps[x]++;
+					removed.push({
+						x : x, 
+						y : y,
+						type : getJewel(x, y)
+					});
+				} else if (gaps[x] > 0) {
+					moved.push({
+						toX : x,
+						toY : y, + gaps[x],
+						fromX : x,
+						fromY : y,
+						type : getJewel(x,y) 
+					});
+					jewels[x][y + gaps[x]] = getJewel(x,y);
+				}
+			}
+		}
+	}
+
 	return {
 		initialize: initialize,
 		print: print,
