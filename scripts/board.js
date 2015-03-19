62edb1cd8cdeadae9688c080d61d5c86eb115e1e
mxt123 <mxtose@gmail.com>
2015-03-19 09:33:44 +0000

    4-11

--- scripts/board.js
+++ scripts/board.js
@@ -20,6 +20,38 @@ jewel.board = (function(){
 		}
 	}
 
+	// return the number of jewels in the longest chain that includes x,y
+	function checkChain(x,y) {
+		var type = getJewel(x,y),
+			left = 0,
+			right = 0,
+			down = 0,
+			up = 0;
+
+		//look right
+		while (type === getJewel(x  + right + 1, y)) {
+			right ++;
+		}
+
+		//look left
+		while (type === getJewel(x  - left - 1, y)) {
+			left ++;
+		}
+
+		//look up
+		while (type === getJewel(x, y + up + 1)) {
+			up ++;
+		}
+
+		//look down
+		while (type === getJewel(x, y - down - 1)) {
+			down ++;
+		}
+
+		return Math.max(left + 1 + right, up + 1 + down);	
+
+	}
+
 	function fillBoard() {
 		var x, y;
 		jewels = [];
@@ -61,9 +93,37 @@ jewel.board = (function(){
 		console.log(str);
 	}
 
+	function canSwap(x1, y1, x2, y2) {
+		var type1 = getJewel(x1,y1),
+			type2 = getJewel(x2,y2),
+			chain;
+		if (!isAdjacent(x1, y1, x2, y2)) {
+			return false;
+		}
+		
+		//temporarily swap jewels
+		jewels[x1][y1] = type2;
+		jewels[x2][y2] = type1;
+		chain = checkChain(x2, y2) > 2 || checkChain(x1,y1) > 2;
+
+		//swap back
+		jewels[x1][y1] = type1;
+		jewels[x2][y2] = type2;
+
+		return chain;
+	}
+
+	function isAdjacent(x1, y1, x2, y2) {
+		var dx = Math.abs(x1 - x2),
+			dy = Math.abs(y1 - y2);
+		return (dx + dy === 1);
+	}
+
 	return {
 		initialize: initialize,
-		print: print
+		print: print,
+		checkChain: checkChain,
+		canSwap: canSwap
 	};
 
 })();
